0d3ee3ab28aee66ac9fcd9313d6d62fd3a871092
Giovanne <giovanneed@gmail.com>
2020-04-05 17:09:52 -0400

    get picture

--- app/src/main/java/com/giovanne/centennial/objectdetection/MainActivity.kt
+++ app/src/main/java/com/giovanne/centennial/objectdetection/MainActivity.kt
@@ -1,12 +1,76 @@
 package com.giovanne.centennial.objectdetection
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_IMAGE_CAPTURE = 1
+    private var imageData: ByteArray? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cameraButton.setOnClickListener {
+            Log.i("giovanne","Clicked")
+
+            if (checkPermission()) {
+                Log.i("giovanne","PERMISSION_GRANTED")
+                dispatchTakePictureIntent()
+
+            }
+
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+
+        val permission = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.CAMERA)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makeRequest()
+        }
+
+        return (permission == PackageManager.PERMISSION_GRANTED)
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.CAMERA  ),
+            REQUEST_IMAGE_CAPTURE)
+    }
+
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode==REQUEST_IMAGE_CAPTURE){
+            val uri = data?.data
+            if (uri != null) {
+                //createImageData(uri)
+            }
+            var bmp = data?.extras?.get("data") as Bitmap
+            picImageView.setImageBitmap(bmp)
+        }
     }
 }
